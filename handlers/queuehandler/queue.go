257220cb412f4030e3a6baa41ff98627ee98fdc1
Everton <user@MacBook.local>
2020-01-06 21:32:13 -0300

    ok

--- handlers/queuehandler/queue.go
+++ handlers/queuehandler/queue.go
@@ -1,7 +1,14 @@
 package queuehandler
 
 func Enqueue(txt string) bool {
-	if txt == "true"{
+	if txt == "true" {
+		return true
+	}
+	return false
+}
+
+func EnqueueTexts(txt1 string, txt2 string) bool {
+	if txt1 == txt2 {
 		return true
 	}
 	return false
